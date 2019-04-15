45c9468baa738e985bbe5c591ff6e47c0a4da81b
Rafał Markowicz <rafal@codesprinters.com>
2019-04-15 12:52:47 +0200

    Thread local introduced

--- automation/src/test/java/dayone/DayOneTests.java
+++ automation/src/test/java/dayone/DayOneTests.java
@@ -3,17 +3,35 @@ package dayone;
 import driverfactory.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class DayOneTests {
 
+    final ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+
+    @BeforeMethod
+    public void setupDriver() {
+        driver.set(DriverFactory.getDriver());
+    }
+
     @Test
-    public void GoogleTest() {
-        WebDriver driver = DriverFactory.getDriver();
-        driver.get("http://google.com");
-        assert driver.findElements(By.name("q")).size() == 1;
-        driver.quit();
+    public void googleTest() {
+
+        driver.get().navigate().to("http://google.com");
+        assert driver.get().findElements(By.name("q")).size() == 1;
+    }
+
+    @Test
+    public void csTest() {
+        driver.get().get("http://agileszkolenia.pl");
+        assert driver.get().findElements(By.id("mce-FNAME")).size() == 1;
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        driver.get().quit();
     }
 
 }
--- automation/src/test/java/driverfactory/DriverFactory.java
+++ automation/src/test/java/driverfactory/DriverFactory.java
@@ -2,16 +2,26 @@ package driverfactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import static driverfactory.Browser.*;
+
 
 public abstract class DriverFactory {
     public static WebDriver getDriver() {
 
-        switch (System.getProperty("browser", System.getProperty("default.browser"))) {
-            case "CHROME" :
+        switch (Browser.valueOf(System.getProperty("browser", System.getProperty("default.browser")
+                .toUpperCase()))) {
+            case CHROME :
                 return new ChromeDriver();
-            case "FIREFOX" :
+            case FIREFOX :
                 return new FirefoxDriver();
+            case EDGE :
+                return new EdgeDriver();
+            case MSIE :
+                return new InternetExplorerDriver();
         }
 
         return null;
